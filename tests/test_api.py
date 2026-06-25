5f3f0e9400b94b4f40148a93b366f4dccb5b37ca
Jean Jesus <jesus.jean@yahoo.com.br>
2026-06-24 22:03:13 -0300

    Adding test_predict_invalid_age

--- tests/test_api.py
+++ tests/test_api.py
@@ -94,3 +94,15 @@ def test_predict_missing_filed():
     client = TestClient(app)
     response = client.post("/predict", json=payload)
     assert response.status_code == 422
+
+
+def test_predict_invalid_age():
+    payload = {
+      "age":"banana",
+      "income": 5000,
+      "city": "Sao Paulo"
+    }
+
+    client = TestClient(app)
+    response = client.post("/predict", json=payload)
+    assert response.status_code == 422
